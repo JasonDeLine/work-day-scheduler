22e6fd3ea4e506e0f86495f8e48b156404a300c5
Jason DeLine <jcdeline1982@gmail.com>
2023-03-23 16:12:29 -0500

    updating style, and save button icon

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,6 +1,10 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// in the html.\
+
+var currentDate =
+  moment().format("dddd") + " " + moment().format("Do MMM YYYY");
+var currentHour = moment().format("h:mm:ss a");
 
 // Display current date
 var today = dayjs();
@@ -24,8 +28,8 @@ var hourSpan;
 
 var interval = setInterval(function () {
   var momentNow = moment();
-  $("#currentDay").html(
-    momentNow.format("YYYY MMMM DD") +
+  $("#currentDate").html(
+    momentNow.format("MM/DD/YYYY") +
       " " +
       momentNow.format("dddd").substring(0, 3).toUpperCase()
   );
@@ -36,41 +40,42 @@ function initPage() {
   console.log("Current Hour " + hour);
   var init9 = JSON.parse(localStorage.getItem("09:00 am"));
   nineAm.val(init9);
-}
-function initPage() {
-  console.log("Current Hour " + hour);
-  var init9 = JSON.parse(localStorage.getItem("09:00 am"));
-  nineAm.val(init9);
 
-  var init10 = JSON.parse(localStorage.getItem("10:00 am"));
-  tenAm.val(init10);
+  function initPage() {
+    console.log("Current Hour " + hour);
+    var init9 = JSON.parse(localStorage.getItem("09:00 am"));
+    nineAm.val(init9);
 
-  var init11 = JSON.parse(localStorage.getItem("11:00 am"));
-  elevenAm.val(init11);
+    var init10 = JSON.parse(localStorage.getItem("10:00 am"));
+    tenAm.val(init10);
 
-  var init12 = JSON.parse(localStorage.getItem("12:00 pm"));
-  twelvePm.val(init12);
+    var init11 = JSON.parse(localStorage.getItem("11:00 am"));
+    elevenAm.val(init11);
 
-  var init1 = JSON.parse(localStorage.getItem("01:00 pm"));
-  onePm.val(init1);
+    var init12 = JSON.parse(localStorage.getItem("12:00 pm"));
+    twelvePm.val(init12);
 
-  var init2 = JSON.parse(localStorage.getItem("02:00 pm"));
-  twoPm.val(init2);
+    var init1 = JSON.parse(localStorage.getItem("01:00 pm"));
+    onePm.val(init1);
 
-  var init3 = JSON.parse(localStorage.getItem("03:00 pm"));
-  threePm.val(init3);
+    var init2 = JSON.parse(localStorage.getItem("02:00 pm"));
+    twoPm.val(init2);
 
-  var init4 = JSON.parse(localStorage.getItem("04:00 pm"));
-  fourPm.val(init4);
+    var init3 = JSON.parse(localStorage.getItem("03:00 pm"));
+    threePm.val(init3);
 
-  var init5 = JSON.parse(localStorage.getItem("05:00 pm"));
-  fivePm.val(init5);
+    var init4 = JSON.parse(localStorage.getItem("04:00 pm"));
+    fourPm.val(init4);
 
-  var init6 = JSON.parse(localStorage.getItem("06:00 pm"));
-  sixPm.val(init6);
+    var init5 = JSON.parse(localStorage.getItem("05:00 pm"));
+    fivePm.val(init5);
 
-  var init7 = JSON.parse(localStorage.getItem("07:00 pm"));
-  sevenPm.val(init7);
+    var init6 = JSON.parse(localStorage.getItem("06:00 pm"));
+    sixPm.val(init6);
+
+    var init7 = JSON.parse(localStorage.getItem("07:00 pm"));
+    sevenPm.val(init7);
+  }
 }
 
 // Function to look at current time and compare to time block for style
