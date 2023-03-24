872240068b88677b2858576890aa53ddfdd42fad
Jason DeLine <jcdeline1982@gmail.com>
2023-03-23 19:49:02 -0500

    moving index.html out of file

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -73,21 +73,20 @@ function initPage() {
 }
 
 // Function to look at current time and compare to time block for style
-function background() {
-  $(".form-control").each(function () {
-    var timeTest = parseInt($(this).attr("id"));
-    hour = parseInt(hour);
-    console.log(timeTest);
-    console.log(hour);
-    if (hour > timeTest) {
-      $(this).addClass("past");
-    } else if (hour < timeTest) {
-      $(this).addClass("future");
-    } else {
-      $(this).addClass("present");
-    }
-  });
-}
+$(".form-control").each(function () {
+  var timeTest = parseInt($(this).attr("id"));
+  hour = parseInt(hour);
+  console.log(timeTest);
+  console.log(hour);
+  //      console.log(this);
+  if (hour > timeTest) {
+    $(this).addClass("past");
+  } else if (hour < timeTest) {
+    $(this).addClass("future");
+  } else {
+    $(this).addClass("present");
+  }
+});
 
 $(document).ready(function () {
   initPage();
